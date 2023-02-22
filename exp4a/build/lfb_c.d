6b76bd091b02d0bd4c78620fd0ccee8083936153
Wenhui Lin <wl9aa@virginia.edu>
2023-02-22 21:53:07 +0000

    4a

---
+++ exp4a/build/lfb_c.d
@@ -0,0 +1,2 @@
+build/lfb_c.o: src/lfb.c include/printf.h include/mbox.h \
+ include/rev_uva_logo_color3-resized.h
---
+++ exp4a/src/timer.c
@@ -0,0 +1,79 @@
+#include "utils.h"
+#include "printf.h"
+#include "peripherals/timer.h"
+#include "timer.h"
+#include "sched.h"
+
+#ifdef USE_QEMU
+//unsigned int interval = (1 << 26); // xzl: around 1 sec
+unsigned int interval = (1 << 26); // xzl: around 3 sec
+#else
+//unsigned int interval = 1 * 1000 * 1000; // xzl: around 1 sec
+unsigned int interval = 1 * 1000 * 1000; // xzl: around 3 sec
+#endif
+
+/* 	These are for Arm generic timer. 
+	They are fully functional on both QEMU and Rpi3 
+	Recommended.
+*/
+
+// xzl: CNTFRQ_EL0 reports the frequency of the system count
+static unsigned int read_cntfrq(void)
+{
+	unsigned int val;
+	asm volatile ("mrs %0, cntfrq_el0" : "=r" (val));
+  	return val;
+}
+
+void generic_timer_init ( void )
+{
+	unsigned int freq  = read_cntfrq();
+	printf("System count freq (CNTFRQ) is: %u\n", freq);
+
+	printf("interval is set to: %u\n", interval);
+	gen_timer_init();
+	gen_timer_reset(interval);
+}
+
+extern struct task_struct * task[64];
+void handle_generic_timer_irq( void ) 
+{
+	printf("Timer interrupt received. next in %u ticks\n\r", interval);
+	gen_timer_reset(interval);
+	for (int i=1; i<10; i++){
+		//task_struct p = task[i];
+		if (task[i] && task[i]->delay_sec > 0) {
+			task[i]->delay_sec = task[i]->delay_sec -1;
+		}
+		if (task[i]->delay_sec <= 0){
+			task[i]->state = 0;
+		}
+	}
+}
+
+
+/* 
+	These are for "System Timer". They are NOT in use by this project. 
+	I leave the code here FYI. 
+	Rpi3: System Timer works fine. Can generate intrerrupts and be used as a counter for timekeeping.
+	QEMU: System Timer can be used for timekeeping. Cannot generate interrupts. 
+		You may want to adjust @interval as needed
+	cf: 
+	https://fxlin.github.io/p1-kernel/exp3/rpi-os/#fyi-other-timers-on-rpi3
+*/
+unsigned int curVal = 0;
+
+void timer_init ( void )
+{
+	curVal = get32(TIMER_CLO);
+	curVal += interval;
+	put32(TIMER_C1, curVal);
+}
+
+void handle_timer_irq( void ) 
+{
+	curVal += interval;
+	put32(TIMER_C1, curVal);
+	put32(TIMER_CS, TIMER_CS_M1);
+	printf("Timer interrupt received\n\r");
+}
